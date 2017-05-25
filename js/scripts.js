c5c40de0c23720655cc69b5501874c022b87263a
Corey Redding and Daniela Trulls <corey+daniela@>
2017-05-25 16:43:26 -0700

    edit js and fix solution for sorting data in movie table

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,47 @@
-function Table(movie, year, score) {
-  this.userMovie = movie;
-  this.userYear = year;
-  this.userScore = score;
+var Movie = function(movie, year, score) {
+  this.movie = movie;
+  this.year = year;
+  this.score = score;
+}
+
+var Library = function () {
   this.movieArray = [];
-};
-
-Table.prototype.movieInput = function() {
-  return ("<tr><td>" + this.userMovie + "</td><td>" + this.userYear + "</td><td>" + this.userScore + "</td></tr>")
-};
-
-function resetFields() {
-    $("input#input-movie").val("");
-    $("input#input-year").val("");
-    $("input.input-score").val("");
 }
 
 $(document).ready(function() {
-
-  $("form#newTomato").submit(function(event) {
+  var newLibrary = new Library();
+  $("form#newTomato").submit(function(event){
     event.preventDefault();
 
-    var movie = $("input#input-movie").val();
-    var year = $("input#input-year").val();
-    var score = parseInt($("input#input-score").val());
 
-    var newTomato = new Table(movie, year, score);
-    var movie1 = new Table("Predator", 1987, 98);
-    var movie2 = new Table("Wild Tales", 2014, 95);
-    var movie3 = new Table("Minions", 2015, 35);
-    var movieArray = [movie1, movie2, movie3];
-    movieArray.push(newTomato);
-    // console.log(newTomato);
 
-    $(".user-movie").append("<tr><td>" + movie + "</td><td>" + year + "</td><td>" + score + "</td></tr>");
-    console.log(movieArray);
+    var userMovieName = $("#input-movie").val();
+    var userYear = $("#input-year").val();
+    var userScore = parseInt($("#input-score").val());
 
-    resetFields();
+    var userMovie = new Movie (userMovieName, userYear, userScore);
 
-    document.forms["newTomato"].reset();
+    newLibrary.movieArray.push(userMovie);
 
+    console.log(newLibrary);
+    $(".user-movie").append("<tr> <td>" + userMovie.movie + "</td> <td>" + userMovie.year + "</td> <td>" + userMovie.score + "</td> </tr>");
+
+
+
+
+    $("#sort").click(function(){
+    var sortMovies = newLibrary.movieArray.sort(function(a, b){
+      return a.score - b.score;
+    })
+    $(".user-movie").empty();
+    sortMovies.forEach(function(item){
+      $(".user-movie").append("<tr> <td>" + item.movie + "</td> <td>" + item.year + "</td> <td>" + item.score + "</td> </tr>");
+
+    })
   });
-  $(".user-movie").append("<tr><td id='movie1'>" + "Predator" + "</td><td>" + 1987 + "</td><td>" + 98 + "</td></tr>");
-  $(".user-movie").append("<tr><td id='movie2'>" + "Wild Tales" + "</td><td>" + 2014 + "</td><td>" + 95 + "</td></tr>");
-  $(".user-movie").append("<tr><td id='movie3'>" + "Minions" + "</td><td>" + 2015 + "</td><td>" + 35 + "</td></tr>");
 
 
 
-});
+
+  })
+})
